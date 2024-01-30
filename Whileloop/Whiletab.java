4ad3f322e2d2429d430dcc3b6485ff15e7b371a8
jainish234 <143157651+jainish234@users.noreply.github.com>
2024-01-30 22:25:00 +0530

    Add files via upload

---
+++ Whileloop/Whiletab.java
@@ -0,0 +1,13 @@
+class Whiletab
+{
+    public static void main(String[] args)
+    {
+        int i=1;
+        int no=10;
+        while(i<=no)
+        {
+            System.out.println(no+"*"+i+"="+(no*i));
+            i++;
+        }
+    }
+}
---
+++ Whileloop/Whileuserinput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Whileuserinput{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no:");
+        int no=sc.nextInt();
+        System.out.println("This no is"+no);
+    }
+}
