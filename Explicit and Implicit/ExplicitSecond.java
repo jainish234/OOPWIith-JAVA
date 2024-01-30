d3775ffc6515b54649e5ddcab63776c6fa1e110c
jainish234 <143157651+jainish234@users.noreply.github.com>
2024-01-30 22:26:27 +0530

    Add files via upload

---
+++ Explicit and Implicit/ExplicitSecond.java
@@ -0,0 +1,12 @@
+class ExplicitSecond{
+    public static void main(String[] args){
+        long sq=12;
+        System.out.println("long."+(sq));
+        int sq1=(int)sq;
+        System.out.println("int."+(sq1));
+        short sq2=(short)sq1;
+        System.out.println("short."+(sq2));
+        byte sq3=(byte)sq2;
+        System.out.println("byte."+(sq3));
+    }
+}
